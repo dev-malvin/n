5b33d615745dd38bf14965ddc36ea69fa544d8cb
dev-malvin <malvintech01@gmail.com>
2025-07-18 08:47:24 +0200

    Update yts2.js

--- plugins/yts2.js
+++ plugins/yts2.js
@@ -1,7 +1,7 @@
 const { malvin } = require('../malvin');
 const axios = require('axios');
 const yts = require('yt-search');
-const Config = require('../config');
+const Config = require('../settings');
 
 // Optimized axios instance
 const axiosInstance = axios.create({
